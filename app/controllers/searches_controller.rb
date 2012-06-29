3bbd19c0ef368660bb68e27d7056e9009afdabba
Timothy Clayton <timothy.daniel.clayton@gmail.com>
2012-06-29 09:21:14 -0600

    image now displays thanks to Peter

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,6 +5,8 @@ class SearchesController < ApplicationController
 
   def show
     search = SimpleTwitter::Search.new
+    search.per_page = params[:rpp]
+    search.lang = params[:lang]
     @results = search.search(params[:q])
   end
 end
