eb100e2f95934240bf18b7996f1c750e27c73488
Timothy Clayton <timothy.daniel.clayton@gmail.com>
2012-07-18 17:57:56 -0600

    added regex to sanitize tweets

--- test/unit/tweet_test.rb
+++ test/unit/tweet_test.rb
@@ -10,9 +10,14 @@ class TweetTest < ActiveSupport::TestCase
 
   test("suggested categories works correctly") do
     tweet = Tweet.new do |t|
-      t.tweeted_text = "Business politics is an instance of the class politics #religion"
+      t.tweeted_text = "Politics and Business are instances of the
+      class religion #politics #religion #business? .  !%&"
     end
-assert_equal("Business, Politics, Religion", tweet.suggested_categories)
+    assert_equal("Business, Politics, Religion", tweet.suggested_categories)
   end
 
+  test("a blank string is returned when there are no matches") do
+    tweet = Tweet.new(tweeted_text: "no matching category name")
+    assert_equal("", tweet.suggested_categories)
+  end
 end
