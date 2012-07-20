11a41d1bddc1b467307382ef1794b384e6e6fe8f
Timothy Clayton <timothy.daniel.clayton@gmail.com>
2012-07-20 11:39:54 -0600

    enabled debugger; added test for getting and setting tweet_categories; wrote DRY for tweet method categories_as_string=

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,14 @@
 class Category < ActiveRecord::Base
+  # scope(:with_title, lambda {|t| where('LOWER(title) = ', t.downcase)
   attr_accessible(:title)
   validates(:title, presence: true, uniqueness: true)
   has_and_belongs_to_many(:tweets)
+
+  def self.with_title (title)
+    where('LOWER(title)=?', title.downcase)
+  end
+
+  def title_match? (other)
+    title.downcase == other.downcase
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -15,4 +15,21 @@ class Tweet < ActiveRecord::Base
     title = Category.all.map(&:title).map(&:downcase)
     (text & title).map(&:capitalize).join(', ')
   end
+
+  # Returns a comma separated string of the current category titles
+  def categories_as_string
+    categories.map(&:title).sort.join(', ')
+  end
+
+  #Given a comma separated string of category titles, reset the
+  #categories for this tweet tot he categories in the string
+  def categories_as_string= (new_categories)
+    categories.clear
+
+    new_categories.split(/\s*, \s*/).each do |title|
+      cat = Category.with_title(title).first
+      # if cat not equal to nil is what the below code says
+      categories << cat if cat # could also be followed by !cat.nil?
+    end
+  end
 end
